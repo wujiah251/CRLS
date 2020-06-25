e4bba8d90fd6226f1129dc35a18b03fc8b0f61bf
WuJiahao <wjh15897313880@sjtu.edu.cn>
2020-06-25 12:44:40 +0800

    模糊排序

--- code/quicksort.py
+++ code/quicksort.py
@@ -1,6 +1,8 @@
 import random
 from time import time
 
+
+
 def quicksort(A,start,end):
     if start>=end:
         return
@@ -65,7 +67,7 @@ def hoare_partition(A,start,end):
             A[j]=A[i]
             j-=1
 
-
+# 非递归实现，栈实现
 def tail_recursive_quicksort(A,start,end):
     stack=[]
     stack.append((start,end))
@@ -77,7 +79,46 @@ def tail_recursive_quicksort(A,start,end):
         stack.append((s,q-1))
         stack.append((q+1,e))
 
-A=[2,8,7,1,3,5,6,4]
-tail_recursive_quicksort(A,0,7)
-print(A)
 
+# 模糊排序
+class interval:
+    def __init__(self,left,right):
+        self.left=left
+        self.right=right
+
+def fuzzy_partition(A,start,end):
+    key=A[end]
+    i=start
+    j=start
+    k=end
+    while j<k:
+        if A[j].right <= key.left:
+            tmp=interval(A[i].left,A[i].right)
+            A[i]=interval(A[j].left,A[j].right)
+            A[j]=interval(tmp.left,tmp.right)
+            i+=1
+            j+=1
+        elif A[j].left >= key.right:
+            tmp=interval(A[k].left,A[k].right)
+            A[k]=interval(A[j].left,A[j].right)
+            A[j]=interval(tmp.left,tmp.right)
+            k-=1
+        else:
+            key.left=A[j].left if A[j].left > key.left else key.left
+            key.right=A[j].right if A[j].left < key.right else key.right
+            j+=1
+    return (i,k)
+
+def fuzzy_quicksort(A,start,end):
+    if start>=end:
+        return
+    a,b=fuzzy_partition(A,start,end)
+    fuzzy_quicksort(A,start,a-1)
+    fuzzy_quicksort(A,b+1,end)
+
+A=[(2,4),(8,10),(7,11),(1,2),(3,7),(5,6),(6,9),(4,7)]
+for i in range(len(A)):
+    A[i]=interval(A[i][0],A[i][1])
+fuzzy_quicksort(A,0,7)
+for i in A:
+    print(i.left,i.right,sep=',',end='  ')
